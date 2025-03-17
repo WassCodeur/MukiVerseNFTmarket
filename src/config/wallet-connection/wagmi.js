2dcefe437c876a6cdd763f3b99262b08b6f0f7e5
WassCodeur <wachioubouraima56@gmail.com>
2025-03-17 12:47:03 +0000

    reorder supported networks and fix transport configuration for wallet connection

--- src/config/wallet-connection/wagmi.js
+++ src/config/wallet-connection/wagmi.js
@@ -2,7 +2,7 @@ import { createConfig, http } from "wagmi";
 import { liskSepolia, holesky, } from "wagmi/chains";
 import { walletConnect } from "wagmi/connectors";
 
-export const supportedNetworks = [holesky, liskSepolia];
+export const supportedNetworks = [liskSepolia, holesky, ];
 
 export const config = createConfig({
     chains: supportedNetworks,
@@ -11,9 +11,9 @@ export const config = createConfig({
         walletConnect({ projectId: import.meta.env.VITE_REOWN_PROJECT_ID }),
     ],
     transports: {
-        
-        [holesky.id]: http(),
         [liskSepolia.id]: http(),
+        [holesky.id]: http(),
+        
         
         
     },
